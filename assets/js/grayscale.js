30151bfd08057123b5f8771c49886b85ddbbf429
Kshitiz Aryal <considerkshitiz@gmail.com>
2015-04-09 11:00:56 +0545

    Map scripts updated

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -168,7 +168,7 @@ function init() {
     var map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
+    var image = '../img/map-marker.png';
     var myLatLng = new google.maps.LatLng(27.7089604, 85.3261328);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
