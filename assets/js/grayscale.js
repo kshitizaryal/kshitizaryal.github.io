40747fe6cbbd9cd1f744f8c8a9dfc980f328141f
Kshitiz Aryal <considerkshitiz@gmail.com>
2015-04-09 10:56:53 +0545

    Assets minified

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -38,10 +38,10 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 13,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(27.7089604, 85.3261328), // Kathmandu, Nepal
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -161,15 +161,15 @@ function init() {
     };
 
     // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+    // We are using a div with id="map-canvas" seen below in the <body>
+    var mapElement = document.getElementById('map-canvas');
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(27.7089604, 85.3261328);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
