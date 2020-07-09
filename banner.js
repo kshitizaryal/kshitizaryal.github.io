5ff50b7e9be9e0887ef15ba5c58e5f377c537cd6
Milan Aryal <considermilan@gmail.com>
2020-07-09 12:10:37 +0545

    minor improvements
    - purge css
    - bug fixes

--- banner.js
+++ banner.js
@@ -1,19 +1,19 @@
-const fs = require("fs");
-const pkg = require("./package.json");
-const filename = "assets/css/style.css";
-const script = fs.readFileSync(filename);
-const padStart = str => ("0" + str).slice(-2);
-const dateObj = new Date();
+const fs = require('fs')
+const pkg = require('./package.json')
+const filename = 'assets/css/style.css'
+const script = fs.readFileSync(filename)
+const padStart = (str) => ('0' + str).slice(-2)
+const dateObj = new Date()
 const date = `${dateObj.getFullYear()}-${padStart(
   dateObj.getMonth() + 1
-)}-${padStart(dateObj.getDate())}`;
+)}-${padStart(dateObj.getDate())}`
 const banner = `/*!
  * ${pkg.title} (${pkg.url})
  * Copyright ${dateObj.getFullYear()} ${pkg.author} (${pkg.author_url})
  * Licensed under ${pkg.license} (${pkg.license_url})
  */
-`;
+`
 
-if (script.slice(0, 3) != "/**") {
-  fs.writeFileSync(filename, banner + script);
+if (script.slice(0, 3) != '/**') {
+  fs.writeFileSync(filename, banner + script)
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,29 @@
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const discardComments = require('postcss-discard-comments');
+const purgecss = require('@fullhuman/postcss-purgecss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const discardComments = require('postcss-discard-comments')
+
+const purgecssConfig = {
+  content: ['!(_site|node_modules)/**/*.+(html|js|md)', '*.html'],
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: [],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+}
 
 module.exports = {
   plugins:
     process.env.NODE_ENV === 'production'
       ? [
           // NODE_ENV=production
+          purgecss(purgecssConfig),
           autoprefixer({ cascade: false }),
-          cssnano(),
+          cssnano({ preset: 'default' }),
           discardComments({ removeAll: true }),
         ]
       : [
           // NODE_ENV=development
+          purgecss(purgecssConfig),
           autoprefixer({ cascade: false }),
         ],
 }
